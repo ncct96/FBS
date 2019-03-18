4ff0209ff60329cedb923a2a7fbb1b9d1ca5f630
ncct96 <30764188+ncct96@users.noreply.github.com>
2019-03-18 22:14:01 +0800

    Created Booking UI

---
+++ FBS/FBS/Booking.vb
@@ -0,0 +1,9 @@
+﻿Public Class Booking
+    Private Sub Booking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
