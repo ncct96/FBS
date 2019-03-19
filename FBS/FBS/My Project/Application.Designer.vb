907b856abd784ce3b005032c2b4a5f81b564b93f
ncct96 <30764188+ncct96@users.noreply.github.com>
2019-03-19 23:32:08 +0800

    Revert "Merge branch 'master' into Nicholas"
    This reverts commit f107c917c9d14192bad4fe32190fd4e0dd9762fa, reversing
    changes made to 27bc0765a4a06cea38bb9d5fa64641aff70dfd31.

--- FBS/FBS/My Project/Application.Designer.vb
+++ FBS/FBS/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FBS.Venue_Info
+            Me.MainForm = Global.FBS.Booking
         End Sub
     End Class
 End Namespace
