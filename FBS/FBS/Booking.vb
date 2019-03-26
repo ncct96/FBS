0463ac6e8bd69776888118e126f5c27535025f79
ncct96 <30764188+ncct96@users.noreply.github.com>
2019-03-26 19:56:12 +0800

    Added booking history

--- FBS/FBS/Booking.vb
+++ FBS/FBS/Booking.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Configuration
 Public Class Booking
     Private Sub Booking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'VenueDS.Venue' table. You can move, or remove it, as needed.
@@ -12,16 +13,16 @@ Public Class Booking
 
     Private Sub cbVenue_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbVenue.SelectedIndexChanged
         Try
-            Dim ds As DataSet
-            Dim connectionString As String = "Data Source=ncct96.database.windows.net;Initial Catalog=FBS;User ID=ncct96;Password=Wmq6783Aew5352;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-            Dim conn As New SqlConnection(connectionString)
+            Dim strConn As String = ConfigurationManager.ConnectionStrings("FBSConnectionString").ConnectionString
+            Dim conn As New SqlConnection(strConn)
+
+            Dim rateSQL As String = "SELECT VenueRate FROM Venue WHERE VenueID = @ID"
+            Dim cmd As New SqlCommand(rateSQL, conn)
+            cmd.Parameters.AddWithValue("@ID", cbVenue.SelectedValue)
+
             conn.Open()
 
-            Dim queryPic As String = "SELECT VenueRate FROM Venue WHERE VenueID = " + cbVenue.SelectedValue
-            Dim comm As New SqlCommand(queryPic, conn)
-            Dim adapter As New SqlDataAdapter(comm)
-            adapter.Fill(ds, "Test")
-            lblRate.DataBindings.Add("Text", dt, "VenueRate")
+            lblRate.Text = "RM " & Format(cmd.ExecuteScalar, "f")
         Catch ex As Exception
 
         End Try
