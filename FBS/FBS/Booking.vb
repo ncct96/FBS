0750eb3bd50edb58fe2ddb9e9a9e6574bcd52c0d
ncct96 <30764188+ncct96@users.noreply.github.com>
2019-04-13 00:21:05 +0800

    Updated with validations Done calculation of payment

--- FBS/FBS/Booking.vb
+++ FBS/FBS/Booking.vb
@@ -1,17 +1,42 @@
 ﻿Imports System.Data.SqlClient
 Imports System.Configuration
 Public Class Booking
+    Dim rate As Double
+    Dim timeSlotCount As Integer
     Private Sub Booking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'VenueDS.Venue' table. You can move, or remove it, as needed.
         Me.VenueTableAdapter.Fill(Me.VenueDS.Venue)
-
+        rate = LoadVenueRate()
+        dtpBooking.MinDate = Date.Today().AddDays(1)
+        dtpBooking.MaxDate = Date.Today().AddDays(10)
     End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
 
     End Sub
 
     Private Sub cbVenue_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbVenue.SelectedIndexChanged
+        rate = LoadVenueRate()
+        timeSlotCount = cblbTimeslot.CheckedItems.Count + 1
+        Dim totalPayment As Double = CalcTotalPymt(rate, timeSlotCount)
+        lblTotalPayment.Text = "RM " & Format(totalPayment, "f")
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub btnBook_Click(sender As Object, e As EventArgs) Handles btnBook.Click
+
+    End Sub
+
+    Function CalcTotalPymt(rate As Double, totalHour As Integer) As Double
+        Dim totalPayment As Double = rate * totalHour
+        Return totalPayment
+    End Function
+
+    Function LoadVenueRate() As Double
+        Dim rate As Double
         Try
             Dim strConn As String = ConfigurationManager.ConnectionStrings("FBSConnectionString").ConnectionString
             Dim conn As New SqlConnection(strConn)
@@ -21,14 +46,23 @@ Public Class Booking
             cmd.Parameters.AddWithValue("@ID", cbVenue.SelectedValue)
 
             conn.Open()
-      
-            lblRate.Text = "RM " & Format(cmd.ExecuteScalar, "f")
+
+            rate = cmd.ExecuteScalar
+
+            lblRate.Text = "RM " & Format(rate, "f")
         Catch ex As Exception
 
         End Try
+
+        Return rate
+    End Function
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
     End Sub
 
-    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
-
+    Private Sub cblbTimeslot_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles cblbTimeslot.ItemCheck
+        Dim totalPayment As Double = CalcTotalPymt(rate, cblbTimeslot.CheckedItems.Count + 1)
+        lblTotalPayment.Text = "RM " & Format(totalPayment, "f")
     End Sub
 End Class
