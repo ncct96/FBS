b168d36c22f3ac16c0607bd09d5b3975e433cd31
Tesla <chiupeeng@gmail.com>
2019-04-12 21:35:40 +0800

    Commit

--- FBS/FBS/AdminVenue.vb
+++ FBS/FBS/AdminVenue.vb
@@ -1,4 +1,4 @@
-﻿Public Class Admin_Venue
+﻿Public Class AdminVenue
     Private DAYS As String() = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
     Private TIME As String() = {"10.00AM", "11.00AM", "12.00PM", "1.00PM", "2.00PM", "3.00PM", "4.00PM", "5.00PM", "6.00PM", "7.00PM", "8.00PM", "9.00PM", "10.00PM"}
     Private Sub UploadPhoto(sender As Object, e As EventArgs) Handles picVenue.Click
