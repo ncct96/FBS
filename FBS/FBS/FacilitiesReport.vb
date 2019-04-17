ded5546c6e120f04bb6e9dfdd586af2a3c20ef0a
Ceekay <33780994+SpadeCk@users.noreply.github.com>
2019-04-17 11:07:07 +0800

    commit

---
+++ FBS/FBS/FacilitiesReport.vb
@@ -0,0 +1,10 @@
+﻿Public Class FacilitiesReport
+    Private Sub FacilitiesReportViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dataset As New BookingDS
+        Dim adapter As New BookingDSTableAdapters.BookingTableAdapter
+        Dim report As New FacilitiesSummary
+        report.SetDataSource(dataset)
+        report.SummaryInfo.ReportTitle = "Facilities Summary Report"
+        FacilitiesReportViewer.ReportSource = report
+    End Sub
+End Class
